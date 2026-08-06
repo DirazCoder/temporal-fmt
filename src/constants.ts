be64149ecaab9539520880f89e9881b61b1e558e
jameswilloton2-hash <jameswilloton2@gmail.com>
2026-08-06 15:31:43 +0100

    Remove comment on format string literals
    Removed comment about format strings.

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,2 @@
-// format strings are short hand-written literals ("yyyy-MM-dd")
 // cap the length so a bug or bad input can't make tokenize() do unbounded work
 export const MAX_FORMAT_LENGTH = 1000;
